4707e41b60bd7d3332ea511abcb957186993d057
Mika Koivusaari <mika.koivusaari@gmail.com>
2016-04-30 21:14:45 +0300

    Added comments

--- read_kWh_pulse.ino
+++ read_kWh_pulse.ino
@@ -1,4 +1,5 @@
 
+//variables used in interrupts need to be declared as volatile
 volatile unsigned long kWhCounter1=0;
 volatile unsigned long kWhCounter2=0;
 
@@ -8,13 +9,11 @@ unsigned long counter2Tmp=0;
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 const int LED = 13;
+//Constants for commands
 const String GET = "GET";
 const String HELP = "HELP";
 
 int MAX_CMD_LENGTH = 10;
-//char cmd[10]; //TODO use constant
-//int cmdIndex=0;
-//char incomingByte;
 
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
@@ -28,12 +27,9 @@ void setup(void)
   inputString.reserve(MAX_CMD_LENGTH);
   pinMode(LED, OUTPUT);     
 
-  //alusta keskeytykset pulssien mittaamiseksi
-  //pinneihin 2 ja 3
-//  attachInterrupt(digitalPinToInterrupt(2), kWhInterrupt1, RISING);
-  attachInterrupt(0, kWhInterrupt1, FALLING);
-//  attachInterrupt(digitalPinToInterrupt(3), kWhInterrupt2, RISING);
-  attachInterrupt(1, kWhInterrupt2, FALLING);
+  //Attach interrupts to pins
+  attachInterrupt(digitalPinToInterrupt(2), kWhInterrupt1, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), kWhInterrupt2, RISING);
 }
 
 void loop(void) {
@@ -66,29 +62,6 @@ void loop(void) {
     inputString = "";
     stringComplete = false;
   }
-
-/*  if (kWhCounter1>0) {
-    for (int i=0;i<10;i++) {
-      digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delay(1000);               // wait for a second
-      digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-      delay(1000);               // wait for a second
-    }
-  }*/
-
-/*  delay(10000);
-        cli(); //disable interrupts
-        counter1Tmp=kWhCounter1; //get interrupt counter to tmp variables
-        kWhCounter1=0;
-        counter2Tmp=kWhCounter2;
-        kWhCounter2=0;
-        sei(); //enable interrupts
-        Serial.print("Counters: ");
-        Serial.print(counter1Tmp);
-        Serial.print(",");
-        Serial.print(counter2Tmp);
-        Serial.println("");        */
-  
 }
 
 /*
@@ -112,12 +85,11 @@ void serialEvent() {
   }
 }
 
+//interrupt routines only increment counters
 void kWhInterrupt1(void){
   kWhCounter1++;
-  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
 }
 
 void kWhInterrupt2(void){
   kWhCounter2++;
-  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
 }
